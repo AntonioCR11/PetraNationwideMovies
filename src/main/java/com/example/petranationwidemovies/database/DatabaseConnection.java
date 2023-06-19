46455a3aec64d87e34996815e110653e4f363581
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-06-19 20:47:54 +0700

    add backed up file sql

--- src/main/java/com/example/petranationwidemovies/database/DatabaseConnection.java
+++ src/main/java/com/example/petranationwidemovies/database/DatabaseConnection.java
@@ -1,7 +1,13 @@
 package com.example.petranationwidemovies.database;
 
+import com.example.petranationwidemovies.custom_ui.Data;
+
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class DatabaseConnection {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -24,4 +30,34 @@ public class DatabaseConnection {
         return conn;
     }
 
+    public static void migrate() {
+        String filePath = "pnmovies.sql";
+        try (Connection conn = DatabaseConnection.getInstance();
+             Statement stmt = conn.createStatement();
+             BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+
+            StringBuilder statementBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty() || line.startsWith("--")) {
+                    continue;
+                }
+                statementBuilder.append(line);
+                if (line.endsWith(";")) {
+                    String sqlStatement = statementBuilder.toString();
+                    stmt.execute(sqlStatement);
+                    statementBuilder.setLength(0);
+                }
+            }
+
+            System.out.println("SQL file executed successfully.");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found: " + filePath, e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: " + filePath, e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
